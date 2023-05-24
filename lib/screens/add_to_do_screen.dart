f3b4467c55b33e07bc70fc60656bfb69bcd3d06a
Kateryna Osypenko <kate.osipenko30@gmail.com>
2023-05-24 16:43:20 +0300

    create input for title

--- lib/screens/add_to_do_screen.dart
+++ lib/screens/add_to_do_screen.dart
@@ -9,39 +9,65 @@ class AddToDo extends StatefulWidget {
 }
 
 class _AddToDoState extends State<AddToDo> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
+
+    String? textValidator(String? value) {
+      if (value == null || value.isEmpty) {
+        return 'Будь ласка, введіть текст';
+      }
+      return null;
+    }
+
     return Scaffold(
-      body: Stack(
-        children: [
-          const Background(),
-          SafeArea(
-            child: Column(
-              children: [
-                Row(
+      body: Stack(children: [
+        const Background(),
+        SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Form(
+                key: _formKey,
+                child: Row(
                   children: [
-                    Container(
-                      height: mediaQuery.size.height * 0.1,
-                      width: mediaQuery.size.width * 0.1,
-                      alignment: Alignment.topLeft,
+                    Padding(
+                      padding:
+                          EdgeInsets.only(left: mediaQuery.size.width * 0.05),
                       child: BackButton(
                         color: Theme.of(context).colorScheme.tertiary,
                       ),
                     ),
-
-                    // Container(
-                    //   color: Colors.amber,
-                    //   height: 450,
-                    //   width: 200,
-                    // )
+                    Container(
+                      width: mediaQuery.size.width * 0.8,
+                      margin:
+                          EdgeInsets.only(left: mediaQuery.size.width * 0.03),
+                      child: TextFormField(
+                        cursorColor: Theme.of(context).colorScheme.onSecondary,
+                        style: Theme.of(context).textTheme.titleMedium,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Назва завдання...',
+                        ),
+                        validator: textValidator,
+                      ),
+                    )
                   ],
                 ),
-              ],
-            ),
-          )
-        ],
-      ),
+              ),
+              Column(
+                children: [
+                  TextFormField(
+                    initialValue: 'Назва завдання...',
+                    decoration: InputDecoration(border: InputBorder.none),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
